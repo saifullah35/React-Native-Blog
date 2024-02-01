1f40a25a758b52daed77ea718064237ae5049772
Saif <saif@Arifs-MBP-4.lan>
2024-02-01 12:50:47 -0500

    moving data with context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,8 +4,10 @@ const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
     return (
-        <BlogContext.Provider>
-            {children}
+        <BlogContext.Provider value={5}>
+           {children}
         </BlogContext.Provider>
     );
 };
+
+export default BlogContext;
