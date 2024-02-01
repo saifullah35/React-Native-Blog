312cdf63b0244b1633e7fa8891e2bd54a3d50771
Saif <saif@Arifs-MBP-4.lan>
2024-02-01 15:57:41 -0500

    making a request

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,10 @@
 import createDataContext from "./createDataContext";
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'get_blogposts':
+            return action.payload;
         case 'edit_blogpost':
             return state.map((blogPost) => {
                 if (blogPost.id === action.payload.id) {
@@ -26,6 +29,14 @@ const blogReducer = (state, action) => {
             ];
         default:
             return state;
+    };
+};
+
+const getBlogPosts = dispatch => {
+    return async () => {
+        const response = await jsonServer.get('./blogposts')
+
+        dispatch({ type: 'get_blogposts', payload: response.data });
     };
 };
 
@@ -58,6 +69,6 @@ const editBlogPost = dispatch => {
 
 export const { Context, Provider } = createDataContext(
     blogReducer, 
-    { addBlogPost, deleteBlogPost, editBlogPost },
+    { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
     [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
 );
