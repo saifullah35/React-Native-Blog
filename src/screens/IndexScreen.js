ce77926d720a8015eca27913c3b78ed2f4a7d297
Saif <saif@Arifs-MBP-4.lan>
2024-02-01 13:54:19 -0500

    navigation on top

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'reac
 import { Context } from "../context/BlogContext";
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
     const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
 
     return (
@@ -14,14 +14,16 @@ const IndexScreen = () => {
                 keyExtractor={(blogPosts) => blogPosts.title}
                 renderItem ={({ item }) => {
                     return (
-                        <View style={styles.row}>
-                            <Text style={styles.title}>
-                                {item.title} - {item.id}
-                            </Text>
-                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                                <Feather style={styles.icon} name="trash" />
-                            </TouchableOpacity>
-                        </View>
+                        <TouchableOpacity onPress={() => navigation.navigate('Show', {id: item.id})}>
+                            <View style={styles.row}>
+                                <Text style={styles.title}>
+                                    {item.title} - {item.id}
+                                </Text>
+                                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                    <Feather style={styles.icon} name="trash" />
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableOpacity>
                     );
                 }}
             />
---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet, View } from 'react-native';
+
+const ShowScreen = () => {
+    return (
+        <View>
+            <Text>Show Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ShowScreen;
