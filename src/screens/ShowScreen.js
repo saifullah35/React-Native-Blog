c81dc265de8a476dc78357effddb46f17af8e782
Saif <saif@Arifs-MBP-4.lan>
2024-02-01 13:58:50 -0500

    Retrieving single parts

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useContext} from "react";
 import { Text, StyleSheet, View } from 'react-native';
+import { Context } from "../context/BlogContext";
 
-const ShowScreen = () => {
+const ShowScreen = ({ navigation }) => {
+    const { state }  = useContext(Context);
+
+    //Search through all the blog posts and find the appropriate one
+    const blogPost = state.find(blogPost => blogPost.id === navigation.getParam('id'));
+
     return (
         <View>
-            <Text>Show Screen</Text>
+            <Text>{blogPosts.title}</Text>
         </View>
     );
 };
