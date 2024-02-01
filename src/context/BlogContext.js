1a8090c283b1d934f2f8c4a54eae341344b88ca4
Saif <saif@Arifs-MBP-4.lan>
2024-02-01 13:00:52 -0500

    adding state in context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-    const blogPosts = [
-        { title: 'Blog Post #1'},
-        { title: 'Blog Post #2'}
-    ];
+    const [ blogPosts, setBlogPosts ] = useState();
+
+    // Goal of this function is to use our setter to add new blog posts
+    const addBlogPost = () => {
+        setBlogPosts([ ...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` } ]);
+    };
 
     return (
-        <BlogContext.Provider value={blogPosts}>
+        <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
            {children}
         </BlogContext.Provider>
     );
