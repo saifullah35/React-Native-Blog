0af46e8638edb4d68bf549be7b45c766d7be32b8
Saif <saif@Arifs-MBP-4.lan>
2024-02-01 14:27:25 -0500

    Navigation on save

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -4,11 +4,10 @@ import { Context } from "../context/BlogContext";
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = ({ navigation }) => {
-    const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
+    const { state, deleteBlogPost } = useContext(BlogContext);
 
     return (
         <View>
-            <Button title="Add Post" onPress={addBlogPost} />
             <FlatList 
                 data={state}
                 keyExtractor={(blogPosts) => blogPosts.title}
