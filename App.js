c824fd842390b685c13bdc14f60178984ec3d16e
Saif <saif@Arifs-MBP-4.lan>
2024-02-01 12:41:35 -0500

    wrapping the navigator

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
+import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import IndexScreen from './src/screens/IndexScreen';
 
 const navigator = createStackNavigator ({
   Index: IndexScreen
-}, {
-  initialRouteName: 'Index',
-  defaultNavigationOptions: {
-    title: 'Blogs'
+}, 
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs'
   } 
-
 });
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />;
+};
